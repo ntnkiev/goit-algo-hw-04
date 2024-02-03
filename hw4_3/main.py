907e7bf43f5c744ca84fabd02434ddad9d8adb4e
Anton Asieiev <ntn.kiev@gmail.com>
2024-02-03 17:11:41 +0200

    beta

--- hw4_3/main.py
+++ hw4_3/main.py
@@ -1,7 +1,7 @@
 from colorama import *
 from pathlib import Path
 
-init(autoreset=True)
+init(autoreset=False)
 
 # print(Style.BRIGHT + "!!!" + Style.RESET_ALL)
 # print(Fore.GREEN + "!!!")
@@ -9,24 +9,29 @@ init(autoreset=True)
 # print(Fore.GREEN + "!!!")
 
 # Створення об'єкту Path для директорії
-directory = Path("C:\SynologyDrive\Work\DALI")
+directory = Path("C:\SynologyDrive\Work\Python_test")
 level = 0
 
 def print_tree(directory):
     global level
     current = []
-    for path in directory.iterdir():
-        # print(path)
-        print("\u2523" if path.is_dir() else "\u2503", "\u2501"*level*4 if path.is_dir() else " "*level*4, Fore.RED + "Folder: " if path.is_dir() else Fore.GREEN + "File: ", path.name)
-        if path.is_dir():
-            level += 1
-            # print("\u2503", " " * level * 4, end = '')
-            print_tree(path)
-            level -= 1
-    return None
+    try:
+        for path in directory.iterdir():
+            # print(path)
+            print(("\u2523" if path.is_dir() else "\u2503") + ("\u2501"*level*4 if path.is_dir() else " "*level*4) + Fore.GREEN + "Folder: " if path.is_dir() else Fore.BLUE + "File: ", path.name + Style.RESET_ALL)
+            if path.is_dir():
+                level += 1
+                # print("\u2503", " " * level * 4, end = '')
+                print_tree(path)
+                level -= 1
+        return None
+    except PermissionError:
+        print(Fore.RED + path + "Permission Denied")
+    except FileNotFoundError:
+        print(Fore.RED + "Error: Directory not found")
 
 def main():
-    print(Fore.YELLOW + "\u2752", Fore.YELLOW + directory.name)
+    print(Fore.YELLOW + "\u2752", Fore.YELLOW + directory.name + Style.RESET_ALL)
     print_tree(directory)
 
 if __name__ == "__main__":
